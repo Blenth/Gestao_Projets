472e5f60388f5c824108bbb312a306ce001e1aba
Blenth <jgui.alves1012@gmail.com>
2025-10-22 09:57:18 -0300

    Injeção de HTML para renderização da lista

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,25 @@ from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 
 
+produtos = [
+        {
+            "nome": "café",
+            "quantidade": 30,
+            "imagem": "https://s2-g1.glbimg.com/cSwyuz3Gg0t6TBKAHzkq68v81r4=/0x0:4479x2686/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2025/A/0/8Tz12FRCKuANsWHn0bYQ/adobestock-1210523825-1-.jpeg"
+        },
+        {
+            "nome": "Leite",
+            "quantidade": 12,
+            "imagem": "https://img.freepik.com/fotos-gratis/um-copo-de-leite-fresco-em-um-fundo-de-madeira_144627-13448.jpg"
+        },
+        {
+            "nome": "Açúcar",
+            "quantidade": 25,
+            "imagem": "https://img.freepik.com/fotos-premium/acucar-em-po-em-colher-de-madeira-e-tigela_88281-3202.jpg"
+        },
+    ]
+
+
 def home(request):
     return render(request, 'Cadastro.html')
 
@@ -10,8 +29,60 @@ def Login(request):
     return render(request, 'Login.html')
 
 def List_Prooduct(request):
-    return render(request, 'List.html')  # ou qualquer template que você tenha
+    produtos = [
+        {
+            "nome": "Café",
+            "quantidade": 30,
+            "imagem": "https://s2-g1.glbimg.com/cSwyuz3Gg0t6TBKAHzkq68v81r4=/0x0:4479x2686/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2025/A/0/8Tz12FRCKuANsWHn0bYQ/adobestock-1210523825-1-.jpeg"
+        },
+        {
+            "nome": "Leite",
+            "quantidade": 12,
+            "imagem": "https://img.freepik.com/fotos-gratis/um-copo-de-leite-fresco-em-um-fundo-de-madeira_144627-13448.jpg"
+        },
+        {
+            "nome": "Açúcar",
+            "quantidade": 25,
+            "imagem": "https://img.freepik.com/fotos-premium/acucar-em-po-em-colher-de-madeira-e-tigela_88281-3202.jpg"
+        },
+        {
+            "nome": "Biscoito",
+            "quantidade": 10,
+            "imagem": "https://img.freepik.com/fotos-gratis/deliciosos-biscoitos-caseiros_144627-17879.jpg"
+        },
+        {
+            "nome": "Arroz",
+            "quantidade": 50,
+            "imagem": "https://img.freepik.com/fotos-gratis/arroz-cru-em-tigela-de-madeira_1150-37517.jpg"
+        },
+        {
+            "nome": "Feijão",
+            "quantidade": 20,
+            "imagem": "https://img.freepik.com/fotos-gratis/feijao-preto-cru-em-tigela-de-madeira_1150-37522.jpg"
+        },
+    ]
 
+    html_cards = "<tr>"
+    for i, p in enumerate(produtos, start=1):
+        html_cards += f"""
+        <td>
+          <div class="card">
+            <img src="{p['imagem']}" alt="{p['nome']}">
+            <h3>{p['nome']}</h3>
+            <p>{p['quantidade']} Und</p>
+            <div class="botoes">
+              <button class="info">INFO</button>
+              <button class="controlar">CONTROLAR</button>
+            </div>
+          </div>
+        </td>
+        """
+        if i % 5 == 0 and i < len(produtos):
+            html_cards += "</tr><tr>"
+
+    html_cards += "</tr>"
+
+    return render(request, 'List.html', {'cards_html': html_cards})
 
 def exibir_produto(request, lanche):
     img = Comida(lanche)
